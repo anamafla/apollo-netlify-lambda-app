a37d72fa9e1036602804413d68f273e90e9ff5f3
Ana Mafla <anamafla@hotmail.com>
2020-09-24 17:34:39 -0500

    Add Apollo Server and Client

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// src/App.js
+import ApolloClient from "apollo-boost";
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql"
+});
 
 class LambdaDemo extends Component {
   constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+    super(props);
+    this.state = { loading: false, msg: null };
   }
 
   handleClick = api => e => {
-    e.preventDefault()
+    e.preventDefault();
 
-    this.setState({ loading: true })
+    this.setState({ loading: true });
     fetch("/.netlify/functions/" + api)
       .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+  };
 
   render() {
-    const { loading, msg } = this.state
+    const { loading, msg } = this.state;
 
     return (
       <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
+        <button onClick={this.handleClick("hello")}>
+          {loading ? "Loading..." : "Call Lambda"}
+        </button>
+        <button onClick={this.handleClick("async-dadjoke")}>
+          {loading ? "Loading..." : "Call Async Lambda"}
+        </button>
         <br />
         <span>{msg}</span>
       </p>
-    )
+    );
   }
 }
 
@@ -43,8 +53,8 @@ class App extends Component {
           <LambdaDemo />
         </header>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
